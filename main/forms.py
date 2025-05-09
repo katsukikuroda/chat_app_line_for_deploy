632b549442f46df643d08b6786e1e3ff4e1883bf
katsukikuroda <katsukikuroda@gmail.com>
2025-05-10 05:38:42 +0900

    1stcmt

---
+++ main/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm  # AuthenticationForm を追加
+from .models import Talk, User
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email",)
+
+class LoginForm(AuthenticationForm):
+    pass
+
+class TalkForm(forms.ModelForm):
+    class Meta:
+        model = Talk
+        fields = ("message",)
+
+class UsernameChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("username",)
+        labels = {"username": "新しいユーザー名"}
+        help_texts = {"username": ""}
+
+class EmailChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("email",)
+        labels = {"email": "新しいメールアドレス"}
